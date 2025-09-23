de2e6548de04e32ed1b456191e78a6d717aeb36b
isaac1131 <isaacaning19@gmail.com>
2025-09-23 19:57:58 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~M31RUJTb#Il4vVTX3C1QXpJHRlzaItoSo573C2pp1wu8nWYM0HKg",  // add your Session Id here
+  OWNER_NUMBER: "", // put your phone number here 233595280924
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
